6b0cb9e45a898ef94b8310d78d2f94e3ba987872
Shaffira Alya Mevia <alyameviia@gmail.com>
2020-11-23 16:22:46 +0700

    fixed gacha system, updated items

--- src/shop.pl
+++ src/shop.pl
@@ -5,14 +5,14 @@
 % Testing Purposes 
 :- dynamic(player_gold/1).
 :- dynamic(potion/2).
-:- dynamic(weapon/5).
-:- dynamic(armor/5).
-:- dynamic(accessory/5).
-player_gold(1000).
+:- dynamic(weapon/6).
+:- dynamic(armor/6).
+:- dynamic(accessory/6).
+player_gold(10000).
 player_job(2).
-weapon('Wooden Sword', 1, 0, 0, 0).
-armor('Iron Armor', 1, 0, 0, 0).
-accessory('Wood Talisman', 1, 0, 0, 0). 
+weapon(0, 'Wooden Sword', 1, 0, 0, 0).
+armor(0, 'Iron Armor', 1, 0, 0, 0).
+accessory(0, 'Wood Talisman', 1, 0, 0, 0). 
 */
 
 /* -----[ SHOP ] ----- */
@@ -47,47 +47,68 @@ exitShop :-
 % Job ID :  1 - Swordsman
 %           2 - Archer
 %           3 - Sorcerer
+% Details : ITEM LIST
+:- dynamic(weapon_stock/1).
+:- dynamic(armor_stock/1).
+:- dynamic(accessory_stock/1).
 % Details : ITEM ID, NAME, JOB ID, HEALTH, ATTACK, DEFENSE
+:- dynamic(weapon_loot/6).
+:- dynamic(armor_loot/6).
+:- dynamic(accessory_loot/6).
+
 % ----- { ITEM LIST } -----
-% Note : Ini bukan starter pack
+% Note : 0 - Starter Item
+%        1,2,3 - Loot Item 
+weapon_stock([1, 2, 3]).
+armor_stock([1, 2, 3]).
+accessory_stock([1, 2, 3]).
 % ----- Weapon -----
 % Swordsman
+weapon_loot(0, 'Wooden Sword', 1, 0, 0, 0).
 weapon_loot(1, 'Dull Blade', 1, 0, 25, 0).
 weapon_loot(2, 'Sacrificial Sword', 1, 0, 100, 0).
 weapon_loot(3, 'Aquila Favonia', 1, 0, 250, 0).
 % Archer
+weapon_loot(0, 'Basic Bow', 2, 0, 0, 0).
 weapon_loot(1, 'Hunter\'s Bow', 2, 0, 25, 0).
 weapon_loot(2, 'Blackcliff Warbow', 2, 0, 100, 0).
 weapon_loot(3, 'The Viridescent Hunt', 2, 0, 250, 0).
 % Sorcerer
+weapon_loot(0, 'Old Staff', 3, 0, 0, 0).
 weapon_loot(1, 'Pocket Grimoire', 3, 0, 25, 0).
 weapon_loot(2, 'Favonius Codex', 3, 0, 100, 0).
 weapon_loot(3, 'Lost Prayer to the Sacred Winds', 3, 0, 250, 0).
 
 % ----- Armor -----
 % Swordsman
+armor_loot(0, 'Basic Iron Armor', 1, 0, 0, 0).
 armor_loot(1, 'Cuirass of Ending Whispers', 1, 0, 0, 0).
 armor_loot(2, 'War-Forged Tunic of Heroes', 1, 0, 0, 0).
 armor_loot(3, 'Mithril Battleplate', 1, 0, 0, 0).
 % Archer
+armor_loot(0, 'Basic Leather Armor', 2, 0, 0, 0).
 armor_loot(1, 'Vest of the Falling Sky', 2, 0, 0, 0).
 armor_loot(2, 'Adamantite Chestguard', 2, 0, 0, 0).
 armor_loot(3, 'Slumber Boots', 2, 0, 0, 0).
 % Sorcerer
+armor_loot(0, 'Old Robe', 3, 0, 0, 0).
 armor_loot(1, 'Bloodcursed Robes', 3, 0, 0, 0).
 armor_loot(2, 'Vindicator of Darkness', 3, 0, 0, 0).
 armor_loot(3, 'Mourning Dragon Armor', 3, 0, 0, 0).
 
 % ----- Accessory -----
 % Swordsman
+accessory_loot(0, 'Wood Talisman', 1, 0, 0, 0).
 accessory_loot(1, 'Equinox Fruit', 1, 0, 0, 0).
 accessory_loot(2, 'Hide of Lifeblood', 1, 0, 0, 0).
 accessory_loot(3, 'Mantle of Death', 1, 0, 0, 0).
 % Archer
+accessory_loot(0, 'Robin\'s Hope', 1, 0, 0, 0).
 accessory_loot(1, 'Key of Rain', 2, 0, 0, 0).
 accessory_loot(2, 'Soul Horn', 2, 0, 0, 0).
 accessory_loot(3, 'Petrifying Cup', 2, 0, 0, 0).
 % Sorcerer
+accessory_loot(0, 'Drop of Dragon\'s Blood', 1, 0, 0, 0).
 accessory_loot(1, 'Mirage Elixir', 3, 0, 0, 0).
 accessory_loot(2, 'Domination Crown', 3, 0, 0, 0).
 accessory_loot(3, 'Shard of Riddles', 3, 0, 0, 0).
@@ -111,41 +132,65 @@ gacha :-
 gacha(JobID) :-
     random(1, 3, LootType), 
     LootType =:= 1 ->
-        random(1, 3, Item),
-        weapon_loot(Item, ItemName, JobID, Health, Attack, Defense),
-        retract(weapon(_, _, _, _, _)),
-        asserta(weapon(ItemName, JobID, Health, Attack, Defense)),
+        % Selecting Item from Stock
+        weapon_stock(WeaponStock), length(WeaponStock, Stock),
+        random(1, Stock, NthItem), nth(NthItem, WeaponStock, SelectedItem),
+        % Update Stock Information
+        select(SelectedItem, WeaponStock, UpdatedWeaponStock),
+        weapon(ItemID, _, _, _, _, _), TempList = [ItemID], append(TempList, UpdatedWeaponStock, FinalWeaponStock),
+        retract(weapon_stock(_)), asserta(weapon_stock(FinalWeaponStock)),
+        % Get Item
+        weapon_loot(SelectedItem, ItemName, JobID, Health, Attack, Defense),
+        % Update Player Inventory
+        retract(weapon(ItemID, _, _, _, _, _)),
+        asserta(weapon(Item, ItemName, JobID, Health, Attack, Defense)),
 
         write('Congratulations! You got '), write(ItemName), write('.'), nl,
-        write('--- Item Details ---'), nl,
+        write('--- [ Item Details ] ---'), nl,
         write('Type : Weapon'), nl,
         write('Attack :'), write(Attack), nl
 
     ; random(2, 3, LootType), 
         LootType =:= 2 ->
-            random(1, 3, Item), player_job(JobID),
-            armor_loot(Item, ItemName, JobID, Health, Attack, Defense),
-            retract(armor(_, _, _, _, _)),
-            asserta(armor(ItemName, JobID, Health, Attack, Defense)),
+            % Selecting Item from Stock
+            armor_stock(ArmorStock), length(ArmorStock, Stock),
+            random(1, Stock, NthItem), nth(NthItem, ArmorStock, SelectedItem),
+            % Update Stock Information
+            select(SelectedItem, ArmorStock, UpdatedArmorStock),
+            armor(ItemID, _, _, _, _, _), TempList = [ItemID], append(TempList, UpdatedArmorStock, FinalArmorStock),
+            retract(armor_stock(_)), asserta(armor_stock(FinalArmorStock)),
+            % Get Item
+            armor_loot(SelectedItem, ItemName, JobID, Health, Attack, Defense),
+            % Update Player Inventory
+            retract(armor(ItemID, _, _, _, _, _)),
+            asserta(armor(Item, ItemName, JobID, Health, Attack, Defense)),
 
             write('Congratulations! You got '), write(ItemName), write('.'), nl,
-            write('--- Item Details ---'), nl,
+            write('--- [ Item Details ] ---'), nl,
             write('Type : Armor'), nl,
             write('Health :'), write(Health), nl,
             write('Defense :'), write(Defense), nl
 
     ; random(1, 3, Item), player_job(JobID),
-        accessory_loot(Item, ItemName, JobID, Health, Attack, Defense),
-        retract(accessory(_, _, _, _, _)),
-        asserta(accessory(ItemName, JobID, Health, Attack, Defense)),
+        % Selecting Item from Stock
+        accessory_stock(AccStock), length(AccStock, Stock),
+        random(1, Stock, NthItem), nth(NthItem, AccStock, SelectedItem),
+        % Update Stock Information
+        select(SelectedItem, AccStock, UpdatedAccStock),
+        accessory(ItemID, _, _, _, _, _), TempList = [ItemID], append(TempList, UpdatedAccStock, FinalAccStock),
+        retract(accessory_stock(_)), asserta(accessory_stock(FinalAccStock)),
+        % Get Item
+        accessory_loot(SelectedItem, ItemName, JobID, Health, Attack, Defense),
+        % Update Player Inventory
+        retract(accessory(ItemID, _, _, _, _, _)),
+        asserta(accessory(Item, ItemName, JobID, Health, Attack, Defense)),
 
         write('Congratulations! You got '), write(ItemName), write('.'), nl,
-        write('--- Item Details ---'), nl,
+        write('--- [ Item Details ] ---'), nl,
         write('Type : Accessory'), nl,
         write('Health :'), write(Health), nl,
         write('Attack :'), write(Attack), nl,
         write('Defense :'), write(Defense), nl.
-
 
 /* -----[ POTIONS ] ----- */
 % ----- Equipment Count -----
